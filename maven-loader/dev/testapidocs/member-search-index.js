d5ad94c033d96e6aa04d598fc7765c2e676b5457
hgschmie <hgschmie>
2023-10-23 00:22:15 +0000

    Site checkin for project maven-loader (2.2.1-SNAPSHOT)

--- maven-loader/dev/testapidocs/member-search-index.js
+++ maven-loader/dev/testapidocs/member-search-index.js
@@ -1,1 +1,1 @@
-memberSearchIndex = [{"p":"de.softwareforge.testing.maven","c":"TestMavenArtifactLoader","l":"TestMavenArtifactLoader()","u":"%3Cinit%3E()"}];updateSearchResults();
+memberSearchIndex = [{"p":"de.softwareforge.testing.maven","c":"TestMavenArtifactLoader","l":"setUp()"},{"p":"de.softwareforge.testing.maven","c":"TestMavenArtifactLoader","l":"TestMavenArtifactLoader()","u":"%3Cinit%3E()"}];updateSearchResults();
