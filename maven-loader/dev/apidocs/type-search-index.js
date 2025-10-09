ee059cc05ecc1cadc4522074c91af78a487bda19
Henning P. Schmiedehausen <henning@schmiedehausen.org>
2025-10-08 22:04:41 -0700

    Site checkin for project maven-loader :: code (2.4.2-SNAPSHOT)

--- maven-loader/dev/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"de.softwareforge.testing.maven","l":"MavenArtifactLoader"},{"p":"de.softwareforge.testing.maven","l":"MavenVersionMatchBuilder"}];updateSearchResults();
